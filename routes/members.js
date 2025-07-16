f0b6883c2187373d4c805ee8453d595d2de0bcbf
tanzia mourin <tanziamourin68@gmail.com>
2025-07-16 15:22:15 +0600

    delete comment from member

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,8 @@
-
-import express from 'express';
-import { getMemberByEmail } from '../controllers/memberController.js';
+import express from "express";
+import { getMemberByEmail } from "../controllers/memberController.js";
 
 const router = express.Router();
 
-router.get('/:email', getMemberByEmail); // ✅ এখানে রুট যোগ করো
+router.get("/:email", getMemberByEmail);
 
 export default router;
-
